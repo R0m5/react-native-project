9e0b506ca4ba763f958ffaf4e6b7d2107b4e0db3
R0m5 <rlastovetskyi@gmail.com>
2023-07-10 22:59:35 +0300

    Done

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,11 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
+import { StatusBar } from "expo-status-bar";
+import { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+// import * as Font from "expo-font";
+// import * as SplashScreen from "expo-splash-screen";
+
+import { RegistrationScreen } from "./Screens/RegistrationScreen.js";
+import { LoginScreen } from "./Screens/LoginScreen.js";
 
 export default function App() {
+  const [isReady, setIsReady] = useState(false);
+  const [keyboardShown, setKeyboardShown] = useState(false);
+
+  Keyboard.addListener("keyboardDidShow", () => {
+    setKeyboardShown(true);
+  });
+  Keyboard.addListener("keyboardDidHide", () => {
+    setKeyboardShown(false);
+  });
+
+  const hideKeyboard = () => {
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={hideKeyboard}>
+        <ImageBackground
+          source={require("./assets/bg2x.jpg")}
+          style={styles.bgImage}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <LoginScreen keyboardShown={keyboardShown} />
+            {/* <RegistrationScreen keyboardShown={keyboardShown} /> */}
+            <StatusBar style="auto" />
+          </KeyboardAvoidingView>
+        </ImageBackground>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
@@ -13,8 +73,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+
+  bgImage: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "flex-end",
   },
 });
